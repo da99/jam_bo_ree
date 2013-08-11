6a3ccfe602f1e3f62157554915447450ee8b93d5
da99 <i-hate-spam-1234567@mailinator.com>
2013-08-11 04:06:22 -0500

    Fixed: typos in tests

--- tests/09-data.lua
+++ tests/09-data.lua
@@ -1,17 +1,17 @@
 
-local _      = require("underscore")._
-  , assert = require("assert")
-  , One    = require("../lib/tally_ho").Tally_Ho.new()
-;
+local _   = require("underscore")
+local Jam = require("jam_bo_ree")
+local One = Jam.new()
+
 
 describe( 'data', function ()
 
   it( 'holds changes done in callbacks', function ()
     One:on('change', function (f)
-      f.data.a = 1;
+      f.a = 1
     end)
 
-    local data = {};
+    local data = {}
     One:run('change', data)
 
     assert.equal(data.a, 1)
@@ -19,10 +19,10 @@ describe( 'data', function ()
 
   it( 'merges changes done in callbacks', function ()
     One:on('merge', function (f)
-      f.data.b = 2;
+      f.b = 2
     end)
 
-    local data = {};
+    local data = {}
     One:run('merge', data)
 
     assert.equal(data.b, 2)
@@ -30,10 +30,11 @@ describe( 'data', function ()
 
   it( 'merges multiple objects into the first', function ()
     One:on('multi-merge', function (f)
-      f.data.c = 3;
+      f.c = 3
     end)
 
-    local d1 = {}, d2 = {d=4};
+    local d1 = {}
+    local d2 = {d=4}
     One:run('multi-merge', d1, d2)
 
     assert.same(d1, {c=3, d=4})
