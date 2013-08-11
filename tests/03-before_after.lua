777d9c01a7120ca7c203f0d12d300020872abab5
da99 <i-hate-spam-1234567@mailinator.com>
2013-08-11 03:26:17 -0500

    Fixed: test for before/after

--- tests/03-before_after.lua
+++ tests/03-before_after.lua
@@ -1,36 +1,36 @@
 
-local _      = require("underscore")._
-  , assert = require("assert")
-  , TH     = require("../lib/tally_ho").Tally_Ho.new()
-;
+local _   = require"underscore"
+local Jam = require"jam_bo_ree"
+local TH  = Jam.new()
 
 TH:on('before add', function (o)
-  o.data.result.push(1)
+  _.push(o.result, 1)
 end)
 
 TH:on('before add', function (o)
-  o.data.result.push(2)
+  _.push(o.result, 2)
 end)
 
 TH:on('add', function (o)
-  o.data.result.push(3)
+  _.push(o.result, 3)
 end)
 
 TH:on('after add', function (o)
-  o.data.result.push(4)
+  _.push(o.result, 4)
 end)
 
 TH:on('after add', function (o)
-  o.data.result.push(5)
+  _.push(o.result, 5)
 end)
 
 
 describe( 'hooks', function ()
 
   it( 'runs hooks in defined order', function ()
-      TH:run('add', {result={}}, function (o)
-        assert.same( o.data.result, {1,2,3,4,5})
-      end)
+    local o = {result={}}
+    TH:run('add', o)
+    assert.same( {1,2,3,4,5}, o.result)
   end)
 
 end) -- === end desc
+
